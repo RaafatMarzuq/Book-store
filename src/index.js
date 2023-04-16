9194effed7603c6749b94988718de95f6d08bb2c
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2023-04-16 23:22:42 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
