f66600cf671c18fe71938ad9d01a19c28bc4c233
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2023-04-16 23:24:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
