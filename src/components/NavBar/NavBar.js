e5a98786f43e59084fe2c282024487e91e25fc5f
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2023-04-17 22:12:18 +0300

    make it better

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,16 +4,25 @@ import './NavBar.css'; // import external CSS file
 // import Button from '../Button/Button';
 import { BiCart ,BiBookBookmark } from "react-icons/bi";
 import { BsFillHouseFill } from "react-icons/bs";
-
+import { useHistory } from 'react-router-dom';
 
 
 const NavBar = () => {
+  const history = useHistory();
+
     function onClick(page){
+      const regex = new RegExp(`${process.env.PUBLIC_URL.substring(1)}\\/\\w+`);
+        let m = regex.exec(window.location);
+        if(!m){
+            return;
+        }
+        history.push(history.push(`${process.env.PUBLIC_URL}/`))
+    
       page ==="Home" ?
             window.location.href = '/Book-store/' : 
                  page ==="Books" ?
-                  window.location.href = 'Books' :
-                     window.location.href = 'Cart'
+                 window.open('https://github.com/RaafatMarzuq/Book-store', "/Books") :
+                 window.open('https://github.com/RaafatMarzuq/Book-store', "/Cart")
 
     }
   return (
